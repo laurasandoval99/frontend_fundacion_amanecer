051dafac1aaf7a94fd3b853d973ef9e899756087
DiegoAAA <a.arenasdiego@gmail.com>
2022-06-18 13:12:51 -0500

    Cambio de la tabla

--- FrontEnd/FundacionAmanecer.py
+++ FrontEnd/FundacionAmanecer.py
@@ -20,6 +20,8 @@ app= dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 df= pd.read_csv('Cartera_Final_2.csv',low_memory=False, nrows=10000)
 df.drop(['Unnamed: 0'], axis=1, inplace = True)
 df.reset_index()
+df = df[["NRO SOLICITUD","OBLIGACION","PAGARE","REGION","MONTO","FEC SOLICITUD","VALOR CUOTA"]]
+
 
 #df=px.data.iris()
 app.layout = html.Div(dbc.Container([
