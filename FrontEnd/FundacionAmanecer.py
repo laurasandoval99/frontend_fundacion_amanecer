14c83d90bfd96c7c7b0330eb3df772a7f7273a84
Jorge <jorgehgv33@gmail.com>
2022-06-18 01:43:49 -0500

    table add

--- FrontEnd/FundacionAmanecer.py
+++ FrontEnd/FundacionAmanecer.py
@@ -9,14 +9,18 @@ import numpy as np
 import plotly.graph_objs as go
 import plotly.express as px
 from dash.dependencies import Input, Output
-from plotly.offline import init_notebook_mode,  plot
+# from plotly.offline import init_notebook_mode,  plot
 
-os.chdir("C://Users//MIPC//Desktop//DS4A//FUNDACIÓN AMANECER//Visualización//FrontEnd")
+#os.chdir("C://Users//MIPC//Desktop//DS4A//FUNDACIÓN AMANECER//Visualización//FrontEnd")
+
 
 
 app= dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-df= pd.read_csv('Cartera_Final_2.csv',low_memory=False)
+df= pd.read_csv('Cartera_Final_2.csv',low_memory=False, nrows=10000)
+df.drop(['Unnamed: 0'], axis=1, inplace = True)
+df.reset_index()
+
 #df=px.data.iris()
 app.layout = html.Div(dbc.Container([
     dbc.Row([
@@ -30,7 +34,7 @@ app.layout = html.Div(dbc.Container([
         ])
 
     ],id="header1"),
-        
+
     html.Div(
         dbc.Row([
             dcc.Tabs(id="header_tabs", value='Clients_graph', children=[
@@ -46,15 +50,33 @@ app.layout = html.Div(dbc.Container([
 
 @app.callback(Output('Header_Graph', 'children'),
               Input('header_tabs', 'value'))
+
 def render_content(tab):
     html.Hr(),
     if tab == 'Clients_graph':
         fig= px.scatter(df,x='DIAS VENCIDO', y= 'CP')
+
         return html.Div([
-            html.H3('Clients'),
-            dcc.Graph(
-                figure= fig
-            )
+
+            dbc.Row(children = [
+                dbc.Col(width={"size": 8, "offset": 2}, children =[
+
+                html.H2('Dataset Introduction', style={'textAlign':'center'}),
+
+                dash_table.DataTable(
+                data=df.to_dict('records'),
+                columns=[{'id': c, 'name': c} for c in df.columns],
+                page_size=8,
+                fixed_rows={'headers': True},
+                style_cell={'minWidth': 95, 'maxWidth': 100, 'width': 95}
+            ),
+        ]),
+       ]),
+       html.Br(),
+        html.H3('Capital Vencido vs Dias vencidos', style={'textAlign':'center'}),
+        dcc.Graph(
+            figure= fig
+        )
         ])
        
     elif tab == 'Clients_predictor':
@@ -72,5 +94,7 @@ def render_content(tab):
                 }
             )
         ])
+
+
 if __name__ == '__main__': 
     app.run_server()
