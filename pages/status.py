449965fe4ee34f8b6092f799ad6fa876761e0f62
DiegoAAA <a.arenasdiego@gmail.com>
2022-06-28 17:30:28 -0500

    modificación de las graficas en Status

--- pages/status.py
+++ pages/status.py
@@ -1,6 +1,6 @@
 import dash
 import os 
-from dash import dcc, html, dash_table
+from dash import dcc, html, dash_table, callback
 import dash_bootstrap_components as dbc
 import pandas as pd
 import numpy as np
@@ -19,17 +19,43 @@ df2 = gr.load_data2()
 df_tiempo = gr.porcentajes()
 
 #charts
-def line_chart_cap_vencido(df_tiempo):
-    fig = go.Figure(data = [go.Scatter( x = df_tiempo["FECHA"], y = df_tiempo["CAPITAL_VENCIDO"],\
-        line = dict(color="#EF553B",width=4), text = df_tiempo["CAPITAL_VENCIDO"],name = "Capital Vencido")])
-    fig.update_layout(title= "Capital vencido",
-                      xaxis_title = "Fecha",
-                      yaxis_title = "Capital Vencido")
-    return fig
+
 
 # specific layout for this page
 layout =  dbc.Container([
     dbc.Row([dbc.Col([html.H1(id = "H1", children = "Status")],xl= 6,lg=6,md=12,sm=12,xs=12)], style={"textAlign":"center", "marginTop" : 30, "marginBottom":30}),
-    dbc.Row([dbc.Col([dcc.Graph(id = "line_chart_cap_vencido", figure=line_chart_cap_vencido(df_tiempo))],xl= 6,lg=6,md=12,sm=12,xs=12)], style={"textAlign":"center", "marginTop" : 30, "marginBottom":30})
+    dbc.Row([dbc.Col([html.Div([html.H4("Select charts"),
+    dcc.Graph(id = "line_chart_cap_vencido"),html.P("Select chart:"),
+    dcc.Dropdown(
+        id="ticker",
+        options=["CAPITAL_VENCIDO", "SALDO_TOTAL", "CLIENTES_MORA"],
+        value="CAPITAL_VENCIDO",
+        clearable=False,
+    ),])],xl= 6,lg=6,md=12,sm=12,xs=12)], style={"textAlign":"center", "marginTop" : 30, "marginBottom":30})
     
 ],fluid = True)
+
+@callback(
+    Output("line_chart_cap_vencido","figure"),
+    Input("ticker","value"))
+def line_chart_cap_vencido(ticker):
+    df_tiempo = gr.porcentajes()
+    fig = px.line(df_tiempo, x = "FECHA", y = ticker)
+    fig.update_layout(
+        {
+            'paper_bgcolor' : 'rgb(233,233,233)'
+        }
+    )
+    fig.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=6, label="6m", step="month", stepmode="backward"),
+            dict(count=1, label="YTD", step="year", stepmode="todate"),
+            dict(count=1, label="1y", step="year", stepmode="backward"),
+            dict(count=5, label="5y", step="year", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
+    )
+    return fig 
