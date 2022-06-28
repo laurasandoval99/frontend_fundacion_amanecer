0417e50156064a369ff2cfef6bd207d19bbb462d
DiegoAAA <a.arenasdiego@gmail.com>
2022-06-28 16:18:40 -0500

    agregué una nueva pestaña de Status y el upload

--- graficos.py
+++ graficos.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
 
 def load_data():
    df = pd.read_csv('data/Cartera_Final_2.csv',low_memory=False, nrows=10000)
@@ -26,7 +28,41 @@ def deudores_pen():
     return round(por_deudores,2)
 
 
+def load_data2():
+    df2 = pd.read_csv('data/Data_Final.csv', sep=",",encoding="utf-8",low_memory=False,index_col=0)
+    # Dar formato de fecha
+    df2['FECHA'] = pd.to_datetime(df2['FECHA'], format = "%Y-%m" )
+    return df2
 
+df2 = load_data2()
 
+def indicadores():
+    df_ultimo  =  df2[df2['FECHA'] == df2['FECHA'].max()]
+
+    # Indicador 1: Total capital vencido
+    a = df_ultimo['CAPITAL_VEN'].sum()
+
+    # Indicador 2: Cantidad de clientes que estan en mora
+    b = df_ultimo['DEUDOR'].sum()/df_ultimo['DEUDOR'].shape[0]
+
+    # Indicador 3: Total por pagar
+    c = df_ultimo['SALDO_OBLIGACION'].sum()
+    return df_ultimo
+
+df_ultimo = indicadores()
+
+def porcentajes():
+    df_tiempo = df2.groupby('FECHA').agg(CAPITAL_VENCIDO= ('CAPITAL_VEN','sum'),
+                                     DEUDOR_SUMA = ('DEUDOR','sum'),
+                                     DEUDOR_COUNT= ('DEUDOR','count'),
+                                     SALDO_TOTAL= ('SALDO_OBLIGACION','sum'))
+    #Poner clientes en mora en porcentajes
+    df_tiempo['CLIENTES_MORA'] = df_tiempo['DEUDOR_SUMA']/df_tiempo['DEUDOR_COUNT']
+    df_tiempo = df_tiempo.reset_index()
+    return df_tiempo
+
+df_tiempo = porcentajes()
+
+#sns.lineplot(x='FECHA',y='CAPITAL_VENCIDO',data=df_tiempo,palette='Set1', ci = None, legend='brief')
 
 
--- pages/Carga_Archivos.py
+++ pages/Carga_Archivos.py
@@ -1,24 +1,119 @@
+import base64
+import datetime
+import io
+from click import style
 import dash
 import os 
-from dash import dcc, html, dash_table
+from dash import dcc, html, dash_table, callback
 import dash_bootstrap_components as dbc
 import pandas as pd
 import numpy as np
 import plotly.graph_objs as go
 import plotly.express as px
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import dash_labs as dl
 from dash_labs.plugins.pages import register_page
-import graficos as gr
 
 # dash-labs plugin call, menu name and route
 register_page(__name__, path="/Archivos")
 
 # specific layout for this page
-layout = dbc.Container( children=[
+layout = dbc.Container([
+    dbc.Row([dbc.Col([html.H1(id = "H1", children = "Carga de archivos")],xl= 12,lg=12,md=12,sm=12,xs=12)], style={"textAlign":"center", "marginTop" : 30, "marginBottom":30}),
     dbc.Row([
-        dbc.Col([
-            html.H1(children='Carga de archivos'),
-        ], lg=12),
-    ]),
-])
+        dbc.Col([dcc.Upload(
+            id = "upload-data",
+            children = html.Div([
+                'Arraste o ',
+                html.A('seleccione el archivo ')
+            ]), style = {
+                'width': '98%',
+                'height': '60px',
+                'lineHeight': '60px',
+                'borderWidth': '1px',
+                'borderStyle': 'dashed',
+                'borderRadius': '5px',
+                'textAlign': 'center',
+                'margin': '10px'
+            },
+            # Allow multiple files to be uploaded
+            multiple=True
+        ),
+        html.Div(id='output-div'),
+        html.Div(id='output-datatable')],xl= 12,lg=12,md=12,sm=12,xs=12),
+
+
+        #dbc.Col([],xl= 6,lg=6,md=12,sm=12,xs=12)
+
+    ])
+],fluid = True)
+
+def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')),low_memory=False)
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+
+    return html.Div([
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
+        html.P("Inset X axis data"),
+        dcc.Dropdown(id='xaxis-data',
+                     options=[{'label':x, 'value':x} for x in df.columns]),
+        html.P("Inset Y axis data"),
+        dcc.Dropdown(id='yaxis-data',
+                     options=[{'label':x, 'value':x} for x in df.columns]),
+        html.Button(id="submit-button", children="Create Graph"),
+        html.Hr(),
+
+        dash_table.DataTable(
+            data=df.to_dict('records'),
+            columns=[{'name': i, 'id': i} for i in df.columns],
+            page_size=15
+        ),
+        dcc.Store(id='stored-data', data=df.to_dict('records')),
+
+        html.Hr(),  # horizontal line
+
+  
+      
+    ])
+
+
+@callback(Output('output-datatable', 'children'),
+              Input('upload-data', 'contents'),
+              State('upload-data', 'filename'),
+              State('upload-data', 'last_modified'))
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
+
+
+@callback(Output('output-div', 'children'),
+              Input('submit-button','n_clicks'),
+              State('stored-data','data'),
+              State('xaxis-data','value'),
+              State('yaxis-data', 'value'))
+def make_graphs(n, data, x_data, y_data):
+    if n is None:
+        return dash.no_update
+    else:
+        bar_fig = px.bar(data, x=x_data, y=y_data)
+        # print(data)
+        return dcc.Graph(figure=bar_fig)
+
